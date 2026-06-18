8ca6f66c1d4b6dba420a21e4af5bcd9961d8fe34
bisswas-kamrul <bisswaskamruldev@gmail.com>
2026-06-18 11:49:32 +0600

    user contollar chengel

--- controllersFloder/UserContollar.js
+++ controllersFloder/UserContollar.js
@@ -1,10 +1,18 @@
 const UserList = require("../Moddel/Usershema");
-async function UserContollar (req ,res) {
-     try {
-    const user = await UserList.find({},"_id name lastName email"); // সব user আনবে
+
+async function UserContollar(req, res) {
+  try {
+    const user = await UserList.find(
+      {},
+      "_id name lastName email role status"
+    );
+
     res.json(user);
   } catch (error) {
-    res.status(500).json({ message: "Server error" });
+    res.status(500).json({
+      message: "Server error",
+    });
   }
 }
-module.exports = UserContollar
+
+module.exports = UserContollar;
