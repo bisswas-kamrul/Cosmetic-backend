adc10fd73f0b92d4aaced996846d91d127092e09
bisswas-kamrul <bisswaskamruldev@gmail.com>
2026-07-01 17:18:23 +0600

    haa

--- routes/chatbotRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const chatbotController = require("../controllersFloder/chatbotController");
-
-const chatbotRouter = express.Router();
-
-chatbotRouter.post("/chatbot/message", chatbotController);
-
-module.exports = chatbotRouter;
